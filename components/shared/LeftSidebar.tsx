2e584c759c58f7e86e615c20a0ce076e1c56a342
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-11 22:31:04 +0530

    feat(ui): Add site logo in LeftSidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image"
+
 const LeftSidebar = () => {
   return (
     <div className="sticky left-0 top-0 flex h-screen w-[80px] flex-col items-center justify-between overflow-y-auto border border-r-[1px]">
-      <div>cirQle</div>
+      <div className="pt-3">
+        <Image 
+          alt="logo"
+          src="/assets/logo.svg"
+          width={40}
+          height={40}
+        />
+      </div>
       <div>
         <div className="">icon1</div>
         <div className="">icon2</div>
