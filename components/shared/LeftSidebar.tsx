5fb914d02b2b689ba6cc2023f1a84761df0e6b84
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-11 22:53:21 +0530

    feat(ui): Display User profile image in the LeftSidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,23 +1,23 @@
-import Image from "next/image"
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 const LeftSidebar = () => {
   return (
-    <div className="sticky left-0 top-0 flex h-screen w-[80px] flex-col items-center justify-between overflow-y-auto border border-r-[1px]">
-      <div className="pt-3">
-        <Image 
-          alt="logo"
-          src="/assets/logo.svg"
-          width={40}
-          height={40}
-        />
+    <div className="sticky left-0 top-0 flex h-screen w-[80px] flex-col items-center justify-between overflow-y-auto border border-r-[1px] py-3">
+      <div>
+        <Image alt="logo" src="/assets/logo.svg" width={40} height={40} />
       </div>
       <div>
         <div className="">icon1</div>
         <div className="">icon2</div>
         <div className="">icon3</div>
       </div>
-      <div>Profile</div>
+      <div>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </div>
-  )
-}
-export default LeftSidebar
+  );
+};
+export default LeftSidebar;
