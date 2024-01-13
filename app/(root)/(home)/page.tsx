3abc226c3e6e17a4b32b77b333570ffdda6668d3
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-13 12:20:37 +0530

    feat(ui): Implement basic layout of Issue page
    * Issue page displays necessary information of a issue like title, author, date, progress, type, issue, etc.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,33 @@
+import Tag from "@/components/Tag";
+
 const Page = () => {
-  return <div className="h-screen">Page</div>;
+  return (
+    <div className="left-0 top-0 flex h-screen p-6">
+      <div className="flex flex-col">
+        <div className="flex">
+          <Tag />
+          <div className="ml-5">Project Name</div>
+        </div>
+
+        <div className="mt-7 flex">
+          <div className="">Author name - Date</div>
+          <div className="ml-7 flex gap-4">
+            <Tag />
+            <Tag />
+            <Tag />
+          </div>
+        </div>
+
+        <div className="mt-7 ">
+          <div className="font-semibold">Issue</div>
+          <div className="mt-5">Describe the issue</div>
+        </div>
+        <div className="mt-7 ">
+          <div className="font-semibold">Images</div>
+          <div className="mt-5">images...</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Page;
