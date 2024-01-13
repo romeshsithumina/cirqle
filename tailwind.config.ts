0b944bfd480112c29b725a3461a91d30982d0810
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-13 11:18:59 +0530

    style(font): Set Inter as the default font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-     colors: {
-      red: {
-        primary: "#f53162",
-        strong: "#ff8484",
-        light: "#ffe5e5",
-
+      colors: {
+        red: {
+          primary: "#f53162",
+          strong: "#ff8484",
+          light: "#ffe5e5",
+        },
+        blue: {
+          strong: "#3798ff",
+          light: "#ebf4ff",
+        },
+        green: {
+          strong: "#1cc850",
+          light: "#e8f9ed",
+        },
       },
-      blue: {
-        strong: "#3798ff",
-        light: "#ebf4ff"
-      },
-      green: {
-        strong: "#1cc850",
-        light: "#e8f9ed"
-      }
-     }
+    },
+    fontFamily: {
+      inter: ["var(--font-inter)"],
+      spaceGrotesk: ["var(--font-spaceGrotesk)"],
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
