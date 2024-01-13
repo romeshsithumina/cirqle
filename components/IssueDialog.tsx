9171a55a45dbb7daf23b9d2c94f5bfd1463131d1
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-13 22:42:02 +0530

    feat(ui): Implement IssueDialog modal
    * Used to add an issue.
    * Fields present - title, description, image, assigned to, and priority.
    * IssueDialog modal will pop-up when the user click on Add an issue button in the Navbar

---
+++ components/IssueDialog.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TextField } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState } from "react";
+import ImageUpload from "./ImageUpload";
+import Tag from "./Tag";
+import { Combobox } from "./Combobox";
+
+const IssueDialog = () => {
+  const [age, setAge] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Add an Issue</Button>
+      </DialogTrigger>
+      <DialogContent className="bg-white sm:max-w-[800px]">
+        <DialogHeader>
+          <DialogTitle>Add an Issue</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid grid-cols-2 gap-4 py-4">
+          <div className="w-full grid-cols-1 items-center gap-4">
+            <TextField
+              id="outlined-basic"
+              label="Title"
+              variant="outlined"
+              className="w-full"
+            />
+          </div>
+          <div className="col-start-1 grid-cols-1 items-center gap-4">
+            <TextField
+              id="outlined-basic"
+              label="Description"
+              variant="outlined"
+              className="w-full"
+              multiline
+              rows={4}
+            />
+          </div>
+          <div className="col-start-2 row-span-2 row-start-1">
+            <ImageUpload />
+          </div>
+          <div className="col-start-2 mt-5 flex items-center justify-center px-5">
+            <InputLabel className="w-[110px]">Priority</InputLabel>
+            <div className="flex w-full gap-4">
+              <div className="flex w-full  justify-around">
+                <Tag />
+                <Tag />
+                <Tag />
+              </div>
+            </div>
+          </div>
+          <div className="col-start-1 row-start-3 w-full">
+            <Combobox />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button className="bg-red-primary text-white" type="submit">
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default IssueDialog;
