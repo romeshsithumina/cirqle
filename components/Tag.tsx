6d376206f614753a8910b41d96ff01bf11030795
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-15 01:10:53 +0530

    feat(style): Improve Tag component for dynamic styling
    * Refactor Tag component to dynamically apply styles based on the tag
    name, providing more flexibility and reusability for different tag
    types.

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,8 +1,44 @@
-const Tag = () => {
+import { capitalizeFirstLetter } from "@/lib/utils";
+
+interface TagProps {
+  name: string;
+  clickable?: boolean;
+}
+
+const Tag = ({ name, clickable }: TagProps) => {
+  const getClassname = () => {
+    switch (name) {
+      case "bug":
+        return "bg-red-light text-red-strong";
+      case "improvement":
+        return "bg-green-light text-green-strong";
+      case "feature":
+        return "bg-blue-light text-blue-strong";
+      case "wip":
+        return "bg-blue-strong text-white";
+      case "done":
+        return "bg-green-strong text-white";
+      case "todo":
+        return "bg-ash-strong text-white";
+      case "high":
+        return "bg-red-light text-red-strong";
+      case "low":
+        return "bg-green-light text-green-strong";
+      case "medium":
+        return "bg-blue-light text-blue-strong";
+    }
+  };
+
   return (
     <div>
-      <div className="rounded-lg border-none bg-green-light px-2 py-1 text-[12px] font-semibold text-green-strong">
-        TODO
+      <div
+        className={`rounded-lg border-none px-2 py-1 text-[12px] font-semibold ${
+          clickable ? "cursor-pointer" : ""
+        }
+        ${getClassname()}
+        `}
+      >
+        {name ? capitalizeFirstLetter(name) : ""}
       </div>
     </div>
   );
