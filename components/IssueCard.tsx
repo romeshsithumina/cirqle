131c6d8e20ff1b631133f9844e7f1a53c5cd3181
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-16 23:58:53 +0530

    feat(ui): Update IssueCard component to use Props
    * Introduces IssueCardProps interface for flexible data handling.
    * Displays issue name, author, type, and status using props.
    * Integrates Tag component for visual indication of issue status.

--- components/IssueCard.tsx
+++ components/IssueCard.tsx
@@ -1,17 +1,36 @@
 import Tag from "./Tag";
 
-const IssueCard = () => {
+interface IssueCardProps {
+  issueName: string;
+  authorName?: string;
+  issueType: string;
+  issuePriority?: string;
+  issueStatus?: string;
+}
+
+const IssueCard = ({
+  issueName,
+  authorName,
+  issueType,
+  issuePriority,
+  issueStatus,
+}: IssueCardProps) => {
   return (
     <div className="h-[90px] border-b-[1px]">
       <div className="flex h-full w-full">
-        <div className="flex h-full flex-col items-center justify-center pl-6">
-          <div className="">Project name</div>
-          <div className="">Author name</div>
+        <div className="flex h-full flex-col justify-center pl-6">
+          <div className="font-medium">{issueName}</div>
+          <div className="flex items-center gap-4 pt-2">
+            <div className="flex-row">
+              <Tag name={issueType} />
+            </div>
+            <div className="flex-row text-sm text-slate-600">{authorName}</div>
+          </div>
         </div>
 
         <div className="ml-auto flex items-center">
           <div className="pr-6">
-            <Tag />
+            <Tag name="wip" />
           </div>
         </div>
       </div>
