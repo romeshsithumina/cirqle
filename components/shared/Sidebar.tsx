b622d5f0f5efe3b346a6420fc07743a5ddb2f9e4
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-17 00:00:27 +0530

    refactor: Update Sidebar to Dynamically Render Issues
    * Update Sidebar component to fetch and render issues dynamically based on the response from the getIssues API call.

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,39 @@
+"use client";
+
+import { getIssues } from "@/lib/actions/getIssues";
+import { useState, useEffect } from "react";
 import IssueCard from "../IssueCard";
 import Navbar from "./Navbar";
 
+interface Issue {
+  id: number;
+  title: string;
+  type: string;
+  author: {
+    name: string;
+  };
+  priority: string;
+  status: string;
+}
+
 const Sidebar = () => {
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    const fetchIssues = async () => {
+      try {
+        const res = await getIssues();
+        if (res) {
+          setIssues(res);
+        }
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchIssues();
+  }, []);
+
   return (
     <>
       <div className=" w-[400px] border-r-[1px]">
@@ -9,16 +41,15 @@ const Sidebar = () => {
           <Navbar />
         </div>
         <div className="max-h-[calc(100vh-67px)] overflow-y-auto">
-          <IssueCard />
-          <IssueCard />
-          <IssueCard />
-          <IssueCard />
-          <IssueCard />
-          <IssueCard />
-          <IssueCard />
-          <IssueCard />
-          <IssueCard />
-          <IssueCard />
+          {issues.map((issue) => (
+            <IssueCard
+              key={issue.id}
+              issueName={issue.title}
+              issueType={issue.type}
+              issueStatus={issue.status}
+              authorName={issue.author.name}
+            />
+          ))}
         </div>
       </div>
     </>
