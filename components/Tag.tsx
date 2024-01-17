6a5670fa313db2c8e58e234c1f2cba9c28d1758f
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-17 14:29:12 +0530

    style(ui): Refactor styles in Tag component
    * Reorganized and enhanced styles in the Tag component for better readability and maintainability.
    * Created separate objects for tag styles and selected styles to improve clarity.
    * Adjusted the classnames dynamically based on the tag name and selection status.
    * Updated display text logic for certain tag names.

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -9,59 +9,62 @@ interface TagProps {
   onTagSelect?: (value: string) => void;
 }
 
+type TagStyles = {
+  [key: string]: string;
+};
+
+type SelectedStyles = {
+  [key: string]: string;
+};
+
 const Tag = ({ name, clickable, selected, onTagSelect }: TagProps) => {
-  const getClassname = () => {
-    switch (name) {
-      case "bug":
-        return "bg-red-light text-red-strong";
-      case "improvement":
-        return "bg-green-light text-green-strong";
-      case "feature":
-        return "bg-blue-light text-blue-strong";
-      case "wip":
-        return "bg-blue-strong text-white";
-      case "done":
-        return "bg-green-strong text-white";
-      case "todo":
-        return "bg-ash-strong text-white";
-      case "high":
-        return "bg-red-light text-red-strong";
-      case "low":
-        return "bg-green-light text-green-strong";
-      case "medium":
-        return "bg-blue-light text-blue-strong";
-    }
+  const tagStyles: TagStyles = {
+    bug: "border-none bg-red-light text-red-strong",
+    improvement: "border-none bg-green-light text-green-strong",
+    feature: "border-none bg-blue-light text-blue-strong",
+    wip: "border-none bg-blue-strong text-white",
+    "wip outline": "border-blue-strong border text-blue-strong",
+    done: "border-none bg-green-strong text-white",
+    "done outline": "border-green-strong border text-green-strong",
+    todo: "border-none bg-ash-strong text-white",
+    "todo outline": "border-ash-strong border text-ash-strong",
+    high: "border-none bg-red-light text-red-strong",
+    low: "border-none bg-green-light text-green-strong",
+    medium: "border-none bg-blue-light text-blue-strong",
   };
 
-  const getSelectedClassname = () => {
-    switch (selected) {
-      case "bug":
-        return "bg-red-strong text-white";
-      case "improvement":
-        return "bg-green-strong text-white";
-      case "feature":
-        return "bg-blue-strong text-white";
-    }
+  const selectedStyles: SelectedStyles = {
+    bug: "bg-red-strong text-white",
+    improvement: "bg-green-strong text-white",
+    feature: "bg-blue-strong text-white",
   };
 
   const handleClick = () => {
     onTagSelect?.(name);
   };
 
+  const getClassName = () => {
+    const baseStyle = "rounded-lg px-2 py-1 text-[12px] font-semibold";
+    const customStyle = clickable ? "cursor-pointer" : "";
+    const tagStyle = tagStyles[name.toLowerCase()] || "";
+    const selectedStyle = selected === name ? selectedStyles[name] || "" : "";
+
+    return `${baseStyle} ${customStyle} ${tagStyle} ${selectedStyle}`;
+  };
+
+  const displayText =
+    name &&
+    (name.includes("wip") || name.includes("done") || name.includes("todo")
+      ? name.toUpperCase().split(" ")[0]
+      : capitalizeFirstLetter(name).split(" ")[0]);
+
   return (
     <div>
-      <div
-        className={`rounded-lg border-none px-2 py-1 text-[12px] font-semibold ${
-          clickable ? "cursor-pointer" : ""
-        }
-        ${getClassname()} 
-        ${name === selected ? getSelectedClassname() : ""}
-        `}
-        onClick={handleClick}
-      >
-        {name ? capitalizeFirstLetter(name) : ""}
+      <div className={getClassName()} onClick={handleClick}>
+        {displayText}
       </div>
     </div>
   );
 };
+
 export default Tag;
