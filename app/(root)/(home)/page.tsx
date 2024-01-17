a6a1ed2c24660b0a2efc17cf9ffb78ba9cc8463f
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-18 00:18:16 +0530

    feat(ui): Implement Issue Display on Home Page
    * Added components for displaying issue details on the home page.
      - PriorityTag for displaying priority with icons.
      - ImageDisplay for handling the display of images and its preview.
    * Improved the layout and styling of the issue details.
    * Included sample data and placeholder text for demonstration purposes.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,30 +1,46 @@
+import ImageDisplay from "@/components/ImageDisplay";
+import PriorityTag from "@/components/PriorityTag";
 import Tag from "@/components/Tag";
 
 const Page = () => {
   return (
-    <div className="left-0 top-0 flex h-screen p-6">
+    <div className="left-0 top-0 flex h-screen px-8 py-6">
       <div className="flex flex-col">
-        <div className="flex">
-          <Tag />
-          <div className="ml-5">Project Name</div>
+        <div className="flex items-center gap-5">
+          <Tag name="improvement" />
+          <div className="text-lg font-semibold">Drag and Drop Shadow</div>
+          <div className="ml-auto opacity-80">
+            <PriorityTag type="high" />
+          </div>
         </div>
 
-        <div className="mt-7 flex">
-          <div className="">Author name - Date</div>
-          <div className="ml-7 flex gap-4">
-            <Tag />
-            <Tag />
-            <Tag />
+        <div className="mt-7 flex items-center">
+          <div className="flex flex-nowrap gap-2 text-[0.87rem]">
+            <span className="">Mark Johnson</span>
+            <span className="">·</span>
+            <span className="text-slate-600">17.01.2024 at 11:00</span>
+          </div>
+          <div className="ml-10 flex gap-4">
+            <Tag name="todo outline" clickable />
+            <Tag name="wip" selected="wip" clickable />
+            <Tag name="done outline" clickable />
           </div>
         </div>
 
         <div className="mt-7 ">
-          <div className="font-semibold">Issue</div>
-          <div className="mt-5">Describe the issue</div>
+          <div className="font-medium">Issue</div>
+          <div className="mt-5 text-sm text-slate-600">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis odit
+            quisquam necessitatibus velit commodi architecto nisi, pariatur
+            accusantium soluta quae maiores, temporibus voluptate nulla labore
+            rem corporis magni voluptates nihil?
+          </div>
         </div>
         <div className="mt-7 ">
-          <div className="font-semibold">Images</div>
-          <div className="mt-5">images...</div>
+          <div className="font-medium">Images</div>
+          <div className="mt-5 text-sm text-slate-600">
+            <ImageDisplay value="/assets/test.png" />
+          </div>
         </div>
       </div>
     </div>
