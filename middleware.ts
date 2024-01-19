d8496a3eb379605071690e17ddb5d00bb8326ee3
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-19 23:49:58 +0530

    style(middleware): Update public routes in authMiddleware
    * Added a public route for "/api/issue/:id" in 'middleware.ts'.

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/issues", "/issue/:id"],
+  publicRoutes: ["/", "/api/issue", "/issue/:id", "/api/issue/:id"],
 });
 
 export const config = {
