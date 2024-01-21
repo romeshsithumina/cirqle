8f584b6b068fb1f9fc36153e653d186331184ace
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-22 03:21:46 +0530

    chore(ui): Refactor Layout Component Structure
    * Introduced a ProjectProvider to encapsulate project-related context logic.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,19 @@
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Sidebar from "@/components/shared/Sidebar";
 import { IssuesProvider } from "@/contexts/IssuesContext";
+import { ProjectProvider } from "@/contexts/ProjectContext";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex">
-      <IssuesProvider>
-        <LeftSidebar />
-        <Sidebar />
-        <div className="grow">{children}</div>
-      </IssuesProvider>
+      <ProjectProvider>
+        <IssuesProvider>
+          <LeftSidebar />
+          <Sidebar />
+          <div className="grow">{children}</div>
+        </IssuesProvider>
+      </ProjectProvider>
     </main>
   );
 };
