2b39c1a0167cd1cef37f18c17a9301500792f72c
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-22 03:06:58 +0530

    feat(context): Implement ProjectContext
    * Added a new context, ProjectContext, for managing the selected project information.
    * Introduced selectedProject state to track the currently selected project.
    * Implemented the updateSelectedProject function to update the selected project within the context.
    * Created a context provider, ProjectProvider, to wrap components with access to project-related information.

---
+++ contexts/ProjectContext.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+interface Project {
+  id: number;
+  name: string;
+}
+
+interface ProjectContextProps {
+  selectedProject: Project | undefined;
+  updateSelectedProject: (selectedProject: Project) => void;
+}
+
+const ProjectContext = createContext<ProjectContextProps | undefined>(
+  undefined
+);
+
+export const ProjectProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [selectedProject, setSelectedProject] = useState<Project | undefined>();
+
+  const updateSelectedProject = (project: Project) => {
+    setSelectedProject(project);
+  };
+
+  return (
+    <ProjectContext.Provider value={{ selectedProject, updateSelectedProject }}>
+      {children}
+    </ProjectContext.Provider>
+  );
+};
+
+export const useProject = () => {
+  const context = useContext(ProjectContext);
+  if (!context) {
+    throw new Error("useProject must be used within an ProjectProvider");
+  }
+  return context;
+};
