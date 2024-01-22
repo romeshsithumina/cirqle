7be618f254776aeaef69c82a90714268cbe0b92b
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-23 01:15:14 +0530

    feat(ui): Introduce EditIssueDialog component
    * Manages the opening and closing of the IssueEditDialog component.
    * Added edit icon for triggering dialog opening.

--- app/(root)/(home)/issue/[id]/IssueClient.tsx
+++ app/(root)/(home)/issue/[id]/IssueClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ImageDisplay from "@/components/ImageDisplay";
+import IssueEditDialog from "@/components/IssueEditDialog";
 import PriorityTag from "@/components/PriorityTag";
 import Tag from "@/components/Tag";
 import { useIssues } from "@/contexts/IssuesContext";
@@ -8,6 +9,7 @@ import { convertDateFormat } from "@/lib/utils";
 import axios from "axios";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
+import { MdDelete, MdEdit } from "react-icons/md";
 
 interface IssueClientProps {
   issue: {
@@ -20,12 +22,13 @@ interface IssueClientProps {
     createdAt: Date;
     author: { name: string };
     project: { title: string };
-    assignedTo: { name: string };
+    assignedTo: { id: number; name: string };
   };
 }
 
 const IssueClient: React.FC<IssueClientProps> = ({ issue }) => {
   const [selectedTag, setSelectedTag] = useState<string>(issue.status);
+  const [editDialogOpen, setEditDialogOpen] = useState(false);
   const { updateIssue } = useIssues();
   const pathname = usePathname();
 
@@ -41,67 +44,87 @@ const IssueClient: React.FC<IssueClientProps> = ({ issue }) => {
     }
   };
 
+  const handleEditClick = () => {
+    setEditDialogOpen(true);
+  };
+
   if (!issue) return <div>Loading...</div>;
 
   return (
-    <div className="left-0 top-0 h-screen px-8 py-6">
-      <div className="flex flex-col">
-        <div className="flex items-center gap-5">
-          <Tag name={issue.type} />
-          <div className="text-lg font-semibold">{issue.title}</div>
-          <div className="ml-auto opacity-80">
-            <PriorityTag type={issue.priority} />
+    <>
+      <IssueEditDialog
+        open={editDialogOpen}
+        issue={issue}
+        onClose={setEditDialogOpen}
+      />
+      <div className="left-0 top-0 h-screen px-8 py-6">
+        <div className="flex flex-col">
+          <div className="flex items-center gap-5">
+            <Tag name={issue.type} />
+            <div className="text-lg font-semibold">{issue.title}</div>
+            <div className="ml-auto opacity-80">
+              <PriorityTag type={issue.priority} />
+            </div>
           </div>
-        </div>
 
-        <div className="mt-4 flex items-center border-b pb-3">
-          <div className="flex flex-nowrap gap-2 text-[0.87rem]">
-            <span className="">{issue.author.name}</span>
-            <span className="">·</span>
-            <span className="text-slate-600">
-              {convertDateFormat(issue.createdAt.toISOString())}
-            </span>
+          <div className="mt-4 flex items-center border-b pb-3">
+            <div className="flex flex-nowrap gap-2 text-[0.87rem]">
+              <span className="">{issue.author.name}</span>
+              <span className="">·</span>
+              <span className="text-slate-600">
+                {convertDateFormat(issue.createdAt.toISOString())}
+              </span>
+            </div>
+            <div className="ml-10 flex gap-4">
+              <Tag
+                name="todo"
+                selected={selectedTag}
+                onTagSelect={handleTagSelect}
+                clickable
+              />
+              <Tag
+                name="wip"
+                selected={selectedTag}
+                onTagSelect={handleTagSelect}
+                clickable
+              />
+              <Tag
+                name="done"
+                selected={selectedTag}
+                onTagSelect={handleTagSelect}
+                clickable
+              />
+            </div>
+            <div className="ml-auto flex gap-4">
+              <MdEdit
+                className="cursor-pointer text-slate-500 hover:text-red-primary"
+                onClick={handleEditClick}
+              />
+              <MdDelete className="cursor-pointer text-slate-500 hover:text-red-primary" />
+            </div>
           </div>
-          <div className="ml-10 flex gap-4">
-            <Tag
-              name="todo"
-              selected={selectedTag}
-              onTagSelect={handleTagSelect}
-              clickable
-            />
-            <Tag
-              name="wip"
-              selected={selectedTag}
-              onTagSelect={handleTagSelect}
-              clickable
-            />
-            <Tag
-              name="done"
-              selected={selectedTag}
-              onTagSelect={handleTagSelect}
-              clickable
-            />
-          </div>
-        </div>
 
-        <div className="mt-7 max-w-[1000px]">
-          <div className="font-medium">Issue</div>
-          <div className="mt-5 text-sm text-slate-600">{issue.description}</div>
-        </div>
-        <div className="mt-7">
-          <div className="font-medium">Images</div>
-          <div className="mt-5 text-sm text-slate-600">
-            <ImageDisplay value="/assets/test.png" />
+          <div className="mt-7 max-w-[1000px]">
+            <div className="font-medium">Issue</div>
+            <div className="mt-5 text-sm text-slate-600">
+              {issue.description}
+            </div>
           </div>
-        </div>
-        <div className="mt-7">
-          <div className="font-medium">Assigned to: </div>
-          <div className="mt-5 text-sm text-slate-600">
-            {issue.assignedTo.name}r
+          <div className="mt-7">
+            <div className="font-medium">Images</div>
+            <div className="mt-5 text-sm text-slate-600">
+              <ImageDisplay value="/assets/test.png" />
+            </div>
+          </div>
+          <div className="mt-7">
+            <div className="font-medium">Assigned to: </div>
+            <div className="mt-5 text-sm text-slate-600">
+              {issue.assignedTo.name}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default IssueClient;
