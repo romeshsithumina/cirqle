32b175965211886ae7b2f26d7837243812be326d
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-24 01:40:14 +0530

    feat(api): Add project creation endpoint

---
+++ app/api/project/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prismadb";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { title, description } = body;
+
+  const newProject = await prisma.project
+    .create({
+      data: {
+        name: title,
+        description,
+      },
+    })
+    .catch(async (e: any) => {
+      console.log(e.response.data);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+
+  console.log(newProject);
+
+  return NextResponse.json(newProject);
+}
