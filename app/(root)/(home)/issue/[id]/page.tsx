860086c677e4439f991d5c57250d5467497e3f65
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-23 18:08:30 +0530

    refactor(ui): Improve Issue Page Component
    * Updated Issue page to fetch issues and re-render page when an issue get updated.

--- app/(root)/(home)/issue/[id]/page.tsx
+++ app/(root)/(home)/issue/[id]/page.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import { getIssueById } from "@/lib/actions/getIssueById";
 import IssueClient from "./IssueClient";
+import { useEffect, useState } from "react";
+import { useIssues } from "@/contexts/IssuesContext";
 
 interface IParams {
   id?: string;
 }
 
-const Page = async ({ params }: { params: IParams }) => {
-  const issue = await getIssueById(params);
+const Page = ({ params }: { params: IParams }) => {
+  const { isIssueUpdated } = useIssues();
+  const [issue, setIssue] = useState();
 
-  if (!issue) <div className="">Loading..</div>;
+  useEffect(() => {
+    const fetchIssues = async () => {
+      const res = await getIssueById(params);
+      setIssue(res);
+    };
+    fetchIssues();
+  }, [isIssueUpdated, params]);
+
+  if (!issue) return <div className="">Loading..</div>;
 
   return <IssueClient issue={issue} />;
 };
