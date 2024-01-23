d4a41e8eb004cf166a396c29ca4e2fab5904f824
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-24 01:45:28 +0530

    feat(ui): Integrate ProjectDialog
    * Added a button to trigger the opening of ProjectCreate dialog for project creation.

--- components/ProjectSelect.tsx
+++ components/ProjectSelect.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
-import * as React from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -16,11 +15,15 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { capitalizeFirstLetter, cn } from "@/lib/utils";
-import { getProjects } from "@/lib/actions/getProjects";
 import { useProject } from "@/contexts/ProjectContext";
+import { getProjects } from "@/lib/actions/getProjects";
+import { capitalizeFirstLetter, cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import ProjectDialog from "./ProjectDialog";
 
 interface ProjectSelectProps {
+  open: boolean;
+  setOpen: (value: boolean) => void;
   onUserSelect: (value: number) => void;
 }
 
@@ -29,61 +32,92 @@ type Project = {
   name: string;
 };
 
-export function ProjectSelect({ onUserSelect }: ProjectSelectProps) {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
-  const [projects, setProjects] = React.useState<Project[]>();
+export function ProjectSelect({
+  open,
+  setOpen,
+  onUserSelect,
+}: ProjectSelectProps) {
+  const [value, setValue] = useState("");
+  const [projectDialogOpen, setProjectDialogOpen] = useState(false);
+  const [projects, setProjects] = useState<Project[]>();
+  const [projectCreated, setProjectCreated] = useState(false);
   const { updateSelectedProject } = useProject();
 
-  React.useEffect(() => {
+  useEffect(() => {
     const fetchProjects = async () => {
       const allProjects = await getProjects();
       setProjects(allProjects);
     };
     fetchProjects();
-  }, []);
+  }, [projectCreated]);
 
   return (
-    <Popover open={open} onOpenChange={setOpen} modal>
-      <PopoverTrigger asChild>
-        <Button
-          variant="secondary"
-          role="combobox"
-          aria-expanded={open}
-          className="max-w-[400px] justify-between text-ellipsis text-lg font-medium"
-        >
-          {value ? capitalizeFirstLetter(value) : "Project..."}
-          <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[170px] bg-white p-0">
-        <Command>
-          <CommandInput placeholder="Search projects..." className="h-9" />
-          <CommandEmpty>No project found.</CommandEmpty>
-          <CommandGroup className="h-[130px] overflow-auto">
-            {projects?.map((project) => (
-              <CommandItem
-                key={project.id}
-                value={project.name}
-                onSelect={(currentValue) => {
-                  setValue(currentValue === value ? "" : currentValue);
+    <>
+      <ProjectDialog
+        projectCreated={projectCreated}
+        setProjectCreated={setProjectCreated}
+        open={projectDialogOpen}
+        onClose={setProjectDialogOpen}
+      />
+      <Popover open={open} onOpenChange={setOpen} modal>
+        <PopoverTrigger asChild>
+          <Button
+            variant="secondary"
+            role="combobox"
+            aria-expanded={open}
+            className="max-w-[400px] justify-between text-ellipsis text-lg font-medium"
+          >
+            {value ? capitalizeFirstLetter(value) : "Project..."}
+            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[170px] bg-white p-0">
+          <Command>
+            <CommandInput placeholder="Search projects..." className="h-9" />
+            <CommandGroup>
+              <Button
+                className="w-full border border-slate-300 bg-white"
+                onClick={() => {
+                  setProjectDialogOpen(true);
                   setOpen(false);
-                  onUserSelect(project.id);
-                  updateSelectedProject(project);
                 }}
               >
-                {project.name}
-                <CheckIcon
-                  className={cn(
-                    "ml-auto h-4 w-4",
-                    value === project.name ? "opacity-100" : "opacity-0"
-                  )}
-                />
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+                Create Project
+              </Button>
+            </CommandGroup>
+
+            <CommandEmpty>No project found.</CommandEmpty>
+            <CommandGroup className="h-[130px] overflow-auto">
+              <span className="flex justify-center border-t-[1px] pt-1 text-sm text-slate-500">
+                Projects
+              </span>
+              {projects?.map((project) => (
+                <CommandItem
+                  key={project.id}
+                  value={project.name}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                    onUserSelect(project.id);
+                    updateSelectedProject(project);
+                  }}
+                  className="hover:text-red-primary"
+                >
+                  {project.name}
+                  <CheckIcon
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      value === project.name.toLocaleLowerCase()
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
