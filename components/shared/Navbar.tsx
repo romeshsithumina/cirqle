721fc47e1d4283a3993a870fafce2a30059f78e0
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-24 01:50:37 +0530

    feat(ui): Add functionality to trigger  ProjectSelect popover when clicked on DotNine icon

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,11 +1,12 @@
+import { useState } from "react";
 import { PiDotsNineBold } from "react-icons/pi";
 import IssueDialog from "../IssueDialog";
 import { ProjectSelect } from "../ProjectSelect";
-import { useState } from "react";
 import { Button } from "../ui/button";
 
 const Navbar = () => {
   const [addDialogOpen, setAddDialogOpen] = useState(false);
+  const [projectSelectOpen, setProjectSelectOpen] = useState(false);
 
   const handleClick = () => {
     setAddDialogOpen(true);
@@ -15,12 +16,24 @@ const Navbar = () => {
     <>
       <IssueDialog open={addDialogOpen} onClose={setAddDialogOpen} />
       <div className="sticky flex h-[65px] flex-1 items-center justify-between border-b-[1px] bg-white">
-        <div className="ml-5 flex flex-row items-center">
-          <PiDotsNineBold color="black" opacity={0.4} size={30} />
-          <ProjectSelect onUserSelect={() => {}} />
+        <div className="ml-5 flex cursor-pointer flex-row items-center">
+          <PiDotsNineBold
+            color="black"
+            opacity={0.4}
+            size={30}
+            onClick={() => setProjectSelectOpen(true)}
+          />
+          <ProjectSelect
+            open={projectSelectOpen}
+            setOpen={setProjectSelectOpen}
+            onUserSelect={() => {}}
+          />
         </div>
         <div className="mr-5">
-          <Button className="border bg-white" onClick={handleClick}>
+          <Button
+            className="border border-red-primary text-red-primary"
+            onClick={handleClick}
+          >
             Add Issue
           </Button>
         </div>
