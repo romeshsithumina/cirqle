d6f104e573bdc7fea340af566a719a97aab8e406
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-31 14:11:35 +0530

    feat(api): Add user creation, update, and deletion endpoints

---
+++ app/api/user/route.ts
@@ -0,0 +1,27 @@
+import prisma from "@/lib/prismadb";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { clerkID, name, email, picture } = body;
+
+  const newUser = await prisma.user
+    .create({
+      data: {
+        clerkID,
+        name,
+        email,
+        picture,
+        role: "developer",
+      },
+    })
+    .catch(async (e: any) => {
+      console.log(e.response.data);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+
+  console.log(newUser);
+
+  return newUser;
+}
