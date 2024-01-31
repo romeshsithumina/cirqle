2760b17d6087a1fd88df0214afca6e0ed3785017
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-31 14:49:53 +0530

    fix(api): Resolve usage of wrong variable name in user creation endpoint

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,12 +3,12 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const { clerkID, name, email, picture } = body;
+  const { clerkId, name, email, picture } = body;
 
   const newUser = await prisma.user
     .create({
       data: {
-        clerkID,
+        clerkID: clerkId,
         name,
         email,
         picture,
